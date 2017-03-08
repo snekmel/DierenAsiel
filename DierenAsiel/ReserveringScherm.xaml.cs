be72b78e12dcbf407416370abdca17115d0b4dc6
Lars <larslemkens@gmail.com>
2017-03-08 11:05:17 +0100

    Bezig met reserveringscherm

--- DierenAsiel/ReserveringScherm.xaml.cs
+++ DierenAsiel/ReserveringScherm.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DierenAsiel.Models;
 
 namespace DierenAsiel
 {
@@ -19,9 +20,37 @@ namespace DierenAsiel
     /// </summary>
     public partial class ReserveringScherm : Window
     {
-        public ReserveringScherm()
+        private MainWindow _mw;
+        private Reservering _selectedReservering;
+
+        public ReserveringScherm(MainWindow mw, Reservering r)
         {
             InitializeComponent();
+            _mw = mw;
+            _selectedReservering = r;
+
+            ViewLoader();
+        }
+
+        private void ViewLoader()
+        {
+            //Vul de reserveringdata
+            ophaaldatumDatepicker.DisplayDate = _selectedReservering.Ophaaldatum;
+
+            //Vul de persoongroupbox
+            naamLabel.Content = _selectedReservering.Persoon.Naam;
+            achternaamLabel.Content = _selectedReservering.Persoon.Achternaam;
+            straatLabel.Content = _selectedReservering.Persoon.Straat;
+            postcodeLabel.Content = _selectedReservering.Persoon.Postcode;
+            woonplaatsLabel.Content = _selectedReservering.Persoon.Woonplaats;
+            telefoonNummerLabel.Content = _selectedReservering.Persoon.Telefoonnummer;
+            emailLabel.Content = _selectedReservering.Persoon.Email;
+
+            //Vul de dierengroupbox
+            dierNaamLabel.Content = _selectedReservering.Dier.Naam;
+            dierSoortLabel.Content = _selectedReservering.Dier.DierType.ToString();
+            dierGebDatumLabel.Content = _selectedReservering.Dier.GeboorteDatum.ToString();
+            dierGeslachtLabel.Content = _selectedReservering.Dier.DierGeslacht.ToString();
         }
     }
 }
