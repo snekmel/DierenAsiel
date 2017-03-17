64408d5ff4b0bce6a5309534ce4411abf083d926
Lars <larslemkens@gmail.com>
2017-03-17 13:49:26 +0100

    Testen toegevoegd.

---
+++ DierenAsiel_UnitTest/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DierenAsiel.Models;
+
+namespace DierenAsiel_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Reservering _r;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _r = new Reservering();
+        }
+
+        [TestMethod]
+        public void CatTester()
+        {
+            Cat k = new Cat();
+            Persoon p = new Persoon();
+            k.ExtraInfo = "hoi";
+            k.VorigeEigenaar = p;
+            Assert.IsNotNull(k.ExtraInfo);
+            Assert.IsNotNull(k.VorigeEigenaar);
+        }
+
+        [TestMethod]
+        public void DogTester()
+        {
+            Dog d = new Dog();
+            Persoon p = new Persoon();
+            d.UitlaatDateTime = DateTime.Now;
+            d.VorigeEigenaar = p;
+            Assert.IsNotNull(d.UitlaatDateTime);
+            Assert.IsNotNull(d.VorigeEigenaar);
+        }
+
+        [TestMethod]
+        public void ReserveringTester()
+        {
+            Dog d = new Dog();
+            Persoon p = new Persoon();
+
+            _r.Persoon = p;
+            _r.Dier = d;
+
+            Assert.IsNotNull(_r.Dier);
+            Assert.IsNotNull(_r.Persoon);
+        }
+    }
+}
